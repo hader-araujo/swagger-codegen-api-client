161ec72fa7316b02f0b2211101e32761ddb0c8b2
Hader <hader.araujo@gmail.com>
2020-06-02 16:45:48 -0300

    models and API generated added

---
+++ src/main/java/br/com/zup/generated/from/swaggercodegen/apiclient/dto/PetDTO.java
@@ -0,0 +1,262 @@
+package br.com.zup.generated.from.swaggercodegen.apiclient.dto;
+
+import java.util.Objects;
+import br.com.zup.generated.from.swaggercodegen.apiclient.dto.CategoryDTO;
+import br.com.zup.generated.from.swaggercodegen.apiclient.dto.TagDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * PetDTO
+ */
+@Validated
+
+public class PetDTO   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("category")
+  private CategoryDTO category = null;
+
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("photoUrls")
+  @Valid
+  private List<String> photoUrls = new ArrayList<>();
+
+  @JsonProperty("tags")
+  @Valid
+  private List<TagDTO> tags = null;
+
+  /**
+   * pet status in the store
+   */
+  public enum StatusEnum {
+    AVAILABLE("available"),
+    
+    PENDING("pending"),
+    
+    SOLD("sold");
+
+    private String value;
+
+    StatusEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static StatusEnum fromValue(String text) {
+      for (StatusEnum b : StatusEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+
+  @JsonProperty("status")
+  private StatusEnum status = null;
+
+  public PetDTO id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public PetDTO category(CategoryDTO category) {
+    this.category = category;
+    return this;
+  }
+
+  /**
+   * Get category
+   * @return category
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public CategoryDTO getCategory() {
+    return category;
+  }
+
+  public void setCategory(CategoryDTO category) {
+    this.category = category;
+  }
+
+  public PetDTO name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  **/
+  @ApiModelProperty(example = "doggie", required = true, value = "")
+  @NotNull
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public PetDTO photoUrls(List<String> photoUrls) {
+    this.photoUrls = photoUrls;
+    return this;
+  }
+
+  public PetDTO addPhotoUrlsItem(String photoUrlsItem) {
+    this.photoUrls.add(photoUrlsItem);
+    return this;
+  }
+
+  /**
+   * Get photoUrls
+   * @return photoUrls
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public List<String> getPhotoUrls() {
+    return photoUrls;
+  }
+
+  public void setPhotoUrls(List<String> photoUrls) {
+    this.photoUrls = photoUrls;
+  }
+
+  public PetDTO tags(List<TagDTO> tags) {
+    this.tags = tags;
+    return this;
+  }
+
+  public PetDTO addTagsItem(TagDTO tagsItem) {
+    if (this.tags == null) {
+      this.tags = new ArrayList<>();
+    }
+    this.tags.add(tagsItem);
+    return this;
+  }
+
+  /**
+   * Get tags
+   * @return tags
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<TagDTO> getTags() {
+    return tags;
+  }
+
+  public void setTags(List<TagDTO> tags) {
+    this.tags = tags;
+  }
+
+  public PetDTO status(StatusEnum status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * pet status in the store
+   * @return status
+  **/
+  @ApiModelProperty(value = "pet status in the store")
+
+
+  public StatusEnum getStatus() {
+    return status;
+  }
+
+  public void setStatus(StatusEnum status) {
+    this.status = status;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PetDTO pet = (PetDTO) o;
+    return Objects.equals(this.id, pet.id) &&
+        Objects.equals(this.category, pet.category) &&
+        Objects.equals(this.name, pet.name) &&
+        Objects.equals(this.photoUrls, pet.photoUrls) &&
+        Objects.equals(this.tags, pet.tags) &&
+        Objects.equals(this.status, pet.status);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, category, name, photoUrls, tags, status);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PetDTO {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    category: ").append(toIndentedString(category)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    photoUrls: ").append(toIndentedString(photoUrls)).append("\n");
+    sb.append("    tags: ").append(toIndentedString(tags)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ src/main/java/br/com/zup/generated/from/swaggercodegen/apiclient/restclient/UserApiClient.java
@@ -0,0 +1,8 @@
+package br.com.zup.generated.from.swaggercodegen.apiclient.restclient;
+
+    import org.springframework.cloud.netflix.feign.FeignClient;
+import io.swagger.configuration.ClientConfiguration;
+
+@FeignClient(name="${swaggerPetstore.name:swaggerPetstore}", url="${swaggerPetstore.url:https://petstore.swagger.io/v2}", configuration = ClientConfiguration.class)
+public interface UserApiClient extends UserApi {
+}
